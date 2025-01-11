e06fe00a91eab219eeb11c7d670ebf5cb2b8abd6
sappe <samuel.lindblom@hotmail.com>
2025-01-11 16:53:49 +0100

    nvim: quickfix no longer have relative numbers

--- nvim/.config/nvim/lua/autocommands.lua
+++ nvim/.config/nvim/lua/autocommands.lua
@@ -4,3 +4,13 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         vim.highlight.on_yank()
     end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+    desc = "Set relative numbers to false for quick fix",
+    callback = function()
+        local ft = vim.o.filetype
+        if ft == "qf" then
+            vim.api.nvim_set_option_value("relativenumber", false, { scope = "local" })
+        end
+    end
+})
