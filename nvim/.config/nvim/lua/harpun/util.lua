5152e79b9b3d43d403c4eba48a9c343d491e4b3c
Samuel Lindblom <samuel.lindblom@stratsys.se>
2025-01-14 14:28:05 +0100

    Revert "nvim: trim filepath in harpun" Can't find buffer when the filename is trimmed. Filename and displayname should be seperate
    This reverts commit 7b01dcf371090f5efb9d548dea34bec2dd118621.

--- nvim/.config/nvim/lua/harpun/util.lua
+++ nvim/.config/nvim/lua/harpun/util.lua
@@ -1,16 +1,7 @@
 local M = {}
 
 function M.get_bufname()
-    local name = M.normalize_path(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), vim.fn.getcwd())
-    local is_trimmed = false
-    while #name > 60 do
-        name = name:sub(10)
-        is_trimmed = true
-    end
-    if is_trimmed then
-        name = "..." .. name
-    end
-    return name
+    return M.normalize_path(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), vim.fn.getcwd())
 end
 
 function M.normalize_path(bufname, root)
