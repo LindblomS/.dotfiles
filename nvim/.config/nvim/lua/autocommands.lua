8955f76f799ed97f32cfbcfcf71109e2326cc016
sappe <samuel.lindblom@hotmail.com>
2025-01-26 21:10:05 +0100

    nvim: Removed r and o from formatoptions

--- nvim/.config/nvim/lua/autocommands.lua
+++ nvim/.config/nvim/lua/autocommands.lua
@@ -19,6 +19,11 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_autocmd("FileType", {
     desc = "Set commentstring depending on file type",
     callback = function()
+        -- Remove autoinsert of commentstring on enter in insert mode.
+        vim.opt.formatoptions = vim.opt.formatoptions - "r"
+        -- Remove autoinsert of commentstring on ctrl-o
+        vim.opt.formatoptions = vim.opt.formatoptions - "o"
+
         local ft = vim.o.filetype
         if ft == "cs" then
             vim.api.nvim_set_option_value("commentstring", "// %s", { scope = "local" })
