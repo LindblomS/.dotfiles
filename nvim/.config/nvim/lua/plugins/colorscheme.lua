0aa6a3936c17da81f0c650fd37a6bfa38748dd4a
Samuel Lindblom <samuel.lindblom@stratsys.se>
2025-03-03 18:34:07 +0100

    nvim: updated colors

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -24,12 +24,12 @@ return {
                     dark = 'dragon',
                 },
                 overrides = function(colors)
-                    local normal = colors.theme.ui.fg
-                    local special = colors.palette.dragonGrey3
+                    local normal = colors.palette.fujiWhite
+                    local special = colors.palette.springViolet1
                     -- return, throw etc
-                    local very_special = colors.palette.dragonRed
-                    local comments = colors.palette.dragonAsh
-                    local types = colors.palette.dragonAqua
+                    local very_special = colors.palette.waveRed
+                    local comments = colors.palette.fujiGrey
+                    local types = colors.palette.waveAqua2
 
                     return {
                         FloatBorder = { bg = "none" },
@@ -37,7 +37,7 @@ return {
                         FloatTitle = { bg = "none" },
                         -- syntax
                         Boolean = { fg = colors.palette.carpYellow, bold = false },
-                        Number = { fg = colors.palette.carpYellow },
+                        Number = { fg = colors.palette.sakuraPink },
                         Constant = { fg = normal },
                         Identifier = { fg = normal },
                         Function = { fg = colors.theme.syn.fun },
@@ -53,6 +53,7 @@ return {
                         Bold = { bold = false },
                         Italic = { italic = false },
                         Comment = { fg = comments },
+                        String = { fg = colors.palette.springGreen },
 
                         -- treesitter
                         ["@variable"] = { fg = normal },
@@ -74,12 +75,13 @@ return {
                         -- c#
                         ["@keyword.operator.c_sharp"] = { link = "Special" },
                         ["@variable.builtin.c_sharp"] = { link = "Special" },
+                        ["@keyword.return.c_sharp"] = { link = "Exception" },
 
                         -- lua
                         ["@keyword.operator.lua"] = { link = "Special" },
                         ["@keyword.return.lua"] = { link = "Exception" },
                         ["@keyword.import.c_sharp"] = { link = "Special" },
-                        ["@constructor.lua"] = { fg = types },
+                        ["@constructor.lua"] = { fg = normal },
 
                         -- rust
                         ["@keyword.return.rust"] = { link = "Exception" },
