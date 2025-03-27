7e05613721fbc10b3a84666b36b6fe7ba2aa9f53
sappe <samuel.lindblom@hotmail.com>
2025-03-27 19:52:17 +0100

    wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -9,72 +9,8 @@ end
 config.window_decorations = "RESIZE"
 
 config.font = wezterm.font("IBM Plex Mono", {})
+config.font_size = 13
 config.line_height = 1.1
-
--- kanagawa wave
--- config.colors = {
---     foreground = "#dcd7ba",
---     background = "#1f1f28",
---
---     cursor_bg = "#c8c093",
---     cursor_fg = "#c8c093",
---     cursor_border = "#c8c093",
---
---     selection_fg = "#c8c093",
---     selection_bg = "#2d4f67",
---
---     scrollbar_thumb = "#16161d",
---     split = "#16161d",
---
---     ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
---     brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
---     indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
---     tab_bar = {
---         background = "#1f1f28",
---         active_tab = {
---             fg_color = "#dcd7ba",
---             bg_color = "#1f1f28"
---         },
---     }
--- }
-
--- kanagawa dragon
-config.colors = {
-    foreground = "#c5c9c5",
-    background = "#181616",
-
-    cursor_bg = "#C8C093",
-    cursor_fg = "#C8C093",
-    cursor_border = "#C8C093",
-
-    selection_fg = "#C8C093",
-    selection_bg = "#2D4F67",
-
-    scrollbar_thumb = "#16161D",
-    split = "#16161D",
-
-    ansi = {
-        "#0D0C0C",
-        "#C4746E",
-        "#8A9A7B",
-        "#C4B28A",
-        "#8BA4B0",
-        "#A292A3",
-        "#8EA4A2",
-        "#C8C093",
-    },
-    brights = {
-        "#A6A69C",
-        "#E46876",
-        "#87A987",
-        "#E6C384",
-        "#7FB4CA",
-        "#938AA9",
-        "#7AA89F",
-        "#C5C9C5",
-    },
-}
-
 
 config.keys = {
     {
@@ -127,6 +63,70 @@ config.keys = {
         mods = "CTRL|SHIFT",
         action = wezterm.action.ScrollByPage(-0.5),
     }
+}
+
+-- kanagawa wave
+-- config.colors = {
+--     foreground = "#dcd7ba",
+--     background = "#1f1f28",
+--
+--     cursor_bg = "#c8c093",
+--     cursor_fg = "#c8c093",
+--     cursor_border = "#c8c093",
+--
+--     selection_fg = "#c8c093",
+--     selection_bg = "#2d4f67",
+--
+--     scrollbar_thumb = "#16161d",
+--     split = "#16161d",
+--
+--     ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+--     brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+--     indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+--     tab_bar = {
+--         background = "#1f1f28",
+--         active_tab = {
+--             fg_color = "#dcd7ba",
+--             bg_color = "#1f1f28"
+--         },
+--     }
+-- }
+
+-- kanagawa dragon
+config.colors = {
+    foreground = "#c5c9c5",
+    background = "#181616",
+
+    cursor_bg = "#c8c093",
+    cursor_fg = "#c8c093",
+    cursor_border = "#c8c093",
+
+    selection_fg = "#c8c093",
+    selection_bg = "#2d4f67",
+
+    scrollbar_thumb = "#16161d",
+    split = "#16161d",
+
+    ansi = {
+        "#0d0c0c",
+        "#c4746e",
+        "#8a9a7b",
+        "#c4b28a",
+        "#8ba4b0",
+        "#a292a3",
+        "#8ea4a2",
+        "#c8c093",
+    },
+    brights = {
+        "#a6a69c",
+        "#e46876",
+        "#87a987",
+        "#e6c384",
+        "#7fb4ca",
+        "#938aa9",
+        "#7aa89f",
+        "#c5c9c5",
+    },
 }
 
 return config
