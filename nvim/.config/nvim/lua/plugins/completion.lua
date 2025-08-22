e1cfb84c0bf1d891c1e0053bab7b0862e1228fd9
Samuel Lindblom <samuel.lindblom@stratsys.se>
2025-08-22 08:52:30 +0200

    nvim: Disable autopairs

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -3,7 +3,7 @@ return {
         "windwp/nvim-autopairs",
         commit = "3d02855468f94bf435db41b661b58ec4f48a06b7",
         event = "InsertEnter",
-        enabled = true,
+        enabled = false,
         config = true,
     },
     {
