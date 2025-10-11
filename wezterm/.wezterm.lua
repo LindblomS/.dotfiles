e8757e59d4ecb49cae6a1036bd92b2264ff9fe9b
sappe <samuel.lindblom@hotmail.com>
2025-10-11 19:36:43 +0200

    wezterm: Changed colors to kanagawa lotus theme, with some customization

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -104,39 +104,114 @@ config.keys = {
 -- }
 
 -- kanagawa dragon
+-- config.colors = {
+--     foreground = "#c5c9c5",
+--     background = "#181616",
+--
+--     cursor_bg = "#c8c093",
+--     cursor_fg = "#c8c093",
+--     cursor_border = "#c8c093",
+--
+--     selection_fg = "#c8c093",
+--     selection_bg = "#2d4f67",
+--
+--     scrollbar_thumb = "#16161d",
+--     split = "#16161d",
+--
+--     ansi = {
+--         "#0d0c0c",
+--         "#c4746e",
+--         "#8a9a7b",
+--         "#c4b28a",
+--         "#8ba4b0",
+--         "#a292a3",
+--         "#8ea4a2",
+--         "#c8c093",
+--     },
+--     brights = {
+--         "#a6a69c",
+--         "#e46876",
+--         "#87a987",
+--         "#e6c384",
+--         "#7fb4ca",
+--         "#938aa9",
+--         "#7aa89f",
+--         "#c5c9c5",
+--     },
+-- }
+
+-- kanagawa lotus
+-- https://github.com/rebelot/kanagawa.nvim/pull/177/files#diff-5c0e0fd16bb531f1b19bf0dddbbb5fc6b9a4cca532d7d648125af8fdd58d7c80
 config.colors = {
-    foreground = "#c5c9c5",
-    background = "#181616",
+    foreground = "#16161D",
+    background = "#f2ecbc",
 
-    cursor_bg = "#c8c093",
-    cursor_fg = "#c8c093",
-    cursor_border = "#c8c093",
+    cursor_bg = "#43436c",
+    cursor_fg = "#43436c",
+    cursor_border = "#43436c",
 
-    selection_fg = "#c8c093",
-    selection_bg = "#2d4f67",
+    selection_fg = "#16161D",
+    selection_bg = "#e4d794",
 
-    scrollbar_thumb = "#16161d",
-    split = "#16161d",
+    scrollbar_thumb = "#b5cbd2",
+    split = "#b5cbd2",
 
     ansi = {
-        "#0d0c0c",
-        "#c4746e",
-        "#8a9a7b",
-        "#c4b28a",
-        "#8ba4b0",
-        "#a292a3",
-        "#8ea4a2",
-        "#c8c093",
+        "#1F1F28",
+        "#c84053",
+        "#6f894e",
+        "#77713f",
+        "#4d699b",
+        "#b35b79",
+        "#597b75",
+        "#545464",
     },
+
     brights = {
-        "#a6a69c",
-        "#e46876",
-        "#87a987",
-        "#e6c384",
-        "#7fb4ca",
-        "#938aa9",
-        "#7aa89f",
-        "#c5c9c5",
+        "#8a8980",
+        "#d7474b",
+        "#6e915f",
+        "#836f4a",
+        "#6693bf",
+        "#624c83",
+        "#5e857a",
+        "#43436c",
+    },
+
+    indexed = {
+        [16] = "#e98a00", -- extended color 1
+        [17] = "#e82424", -- extended color 2
+    },
+
+    tab_bar = {
+        background = "#e4d794",
+
+        active_tab = {
+            bg_color = "#e4d794",
+            fg_color = "#16161D",
+        },
+
+        inactive_tab = {
+            bg_color = "#a6a69c",
+            fg_color = "#e4d794",
+        },
+
+        inactive_tab_hover = {
+            bg_color = "#9fb5c9",
+            fg_color = "#43436c",
+            italic = true,
+        },
+
+        new_tab = {
+            bg_color = "#8a8980",
+            fg_color = "#e4d794",
+        },
+
+        new_tab_hover = {
+            bg_color = "#9fb5c9",
+            fg_color = "#43436c",
+            italic = true,
+        },
     },
 }
 
