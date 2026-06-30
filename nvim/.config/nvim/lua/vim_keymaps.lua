fe39f82159c9061e3f9f6a67f9bac90aefd02259
sappe <samuel.lindblom@hotmail.com>
2026-06-30 20:26:12 +0200

    nvim: Add more substitute keymaps

--- nvim/.config/nvim/lua/vim_keymaps.lua
+++ nvim/.config/nvim/lua/vim_keymaps.lua
@@ -10,7 +10,8 @@ keymap.set('v', '<leader>y', '\"+y')
 keymap.set('n', '<ESC>', ':noh <CR>', { silent = true })
 keymap.set('n', '<C-y>', '<C-y><C-y><C-y>')
 keymap.set('n', '<C-e>', '<C-e><C-e><C-e>')
-local function substitute_word(use_word_boundary)
+
+local function substitute_word(opts)
     local word
     local visual = vim.fn.mode() == "v"
     if visual then
@@ -24,17 +25,33 @@ local function substitute_word(use_word_boundary)
     end
 
     local pattern
-    if use_word_boundary then
+    if opts.use_word_boundary then
         pattern = string.format("\\<%s\\>", word)
     else
         pattern = word
     end
 
-    vim.api.nvim_feedkeys(string.format(":%%s/%s/%s", pattern, word), "n", false)
+    local range
+    if opts.use_visual_range then
+        range = "'<,'>"
+    else
+        range = "%"
+    end
+
+    vim.api.nvim_feedkeys(string.format(":%ss/%s/%s", range, pattern, word), "n", false)
 end
-keymap.set({ "n", "v" }, "<leader>s", function() substitute_word(false) end,
-    { desc = "Substitute word under cursor or selection" })
-keymap.set({ "n", "v" }, "<leader>S", function() substitute_word(true) end,
-    { desc = "Substitute word under cursor or selection using word bounderies" })
+
+keymap.set({ "n", "v" }, "<leader>s", function() substitute_word({}) end,
+    { desc = "Substitute word under cursor or visual selection" })
+
+keymap.set({ "n", "v" }, "<leader>S", function() substitute_word({ use_word_boundary = true }) end,
+    { desc = "Substitute word under cursor or visual selection using word bounderies" })
+
+keymap.set({ "n", "v" }, "<leader>vs", function() substitute_word({ use_visual_range = true }) end,
+    { desc = "In the last visual selection, substitute word under cursor or visual selection" })
+
+keymap.set({ "n", "v" }, "<leader>vS",
+    function() substitute_word({ use_word_boundary = true, use_visual_range = true }) end,
+    { desc = "In the last visual selection, substitute word under cursor or visual selection using word bounderies" })
 
 keymap.set("t", "<M-t>", "<C-\\><C-N>", { desc = "Exit terminal mode" })
