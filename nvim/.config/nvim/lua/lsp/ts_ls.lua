4dac3fb6db41cbe4c1b313ed23b136be506f3c33
sappe <samuel.lindblom@hotmail.com>
2026-07-15 21:48:12 +0200

    nvim: add abbrevations for typescript and javascript

--- nvim/.config/nvim/lua/lsp/ts_ls.lua
+++ nvim/.config/nvim/lua/lsp/ts_ls.lua
@@ -9,6 +9,17 @@ else
     "C:/Users/samuel.lindblom/Appdata/Roaming/npm/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin"
 end
 
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { "typescript", "javascript" },
+    callback = function()
+        -- In .cs files we can add "<esc>T(i" at the end to go back into (). But for some reason it just won't
+        -- work for .ts files.)
+        vim.api.nvim_buf_set_keymap(0, "ia", "print@", "console.log()", {});
+        vim.api.nvim_buf_set_keymap(0, "ia", "aaa@", "// arrange\n// act\n// assert", {});
+    end,
+})
+
 return {
     init_options = {
         plugins = {
@@ -30,73 +41,4 @@ return {
     },
     cmd = { "typescript-language-server", "--stdio" },
     root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
-    docs = {
-        description = [[
-https://github.com/typescript-language-server/typescript-language-server
-
-`ts_ls`, aka `typescript-language-server`, is a Language Server Protocol implementation for TypeScript wrapping `tsserver`. Note that `ts_ls` is not `tsserver`.
-
-`typescript-language-server` depends on `typescript`. Both packages can be installed via `npm`:
-```sh
-npm install -g typescript typescript-language-server
-```
-
-To configure typescript language server, add a
-[`tsconfig.json`](https://www.typescriptlang.org/docs/handbook/tsconfig-json.html) or
-[`jsconfig.json`](https://code.visualstudio.com/docs/languages/jsconfig) to the root of your
-project.
-
-Here's an example that disables type checking in JavaScript files.
-
-```json
-{
-  "compilerOptions": {
-    "module": "commonjs",
-    "target": "es6",
-    "checkJs": false
-  },
-  "exclude": [
-    "node_modules"
-  ]
-}
-```
-
-### Vue support
-
-As of 2.0.0, Volar no longer supports TypeScript itself. Instead, a plugin
-adds Vue support to this language server.
-
-*IMPORTANT*: It is crucial to ensure that `@vue/typescript-plugin` and `volar `are of identical versions.
-
-```lua
-require'lspconfig'.ts_ls.setup{
-  init_options = {
-    plugins = {
-      {
-        name = "@vue/typescript-plugin",
-        location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
-        languages = {"javascript", "typescript", "vue"},
-      },
-    },
-  },
-  filetypes = {
-    "javascript",
-    "typescript",
-    "vue",
-  },
-}
-
--- You must make sure volar is setup
--- e.g. require'lspconfig'.volar.setup{}
--- See volar's section for more information
-```
-
-`location` MUST be defined. If the plugin is installed in `node_modules`,
-`location` can have any value.
-
-`languages` must include `vue` even if it is listed in `filetypes`.
-
-`filetypes` is extended here to include Vue SFC.
-]],
-    },
 }
